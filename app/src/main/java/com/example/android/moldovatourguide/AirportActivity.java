cd0989dd0c75dc1bfa08eec9c84d8e26e7241913
Ghenamd <evaflorn@yahoo.com>
2017-05-02 10:54:56 +0100

    Changed hard coded strings in Java files

--- app/src/main/java/com/example/android/moldovatourguide/AirportActivity.java
+++ app/src/main/java/com/example/android/moldovatourguide/AirportActivity.java
@@ -8,6 +8,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 /**
+ *
  * Created by Ghena on 23/03/2017.
  */
 
@@ -17,12 +18,11 @@ public class AirportActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
-        setTitle("Airport");
+        setTitle(getString(R.string.airport));
 
         ArrayList<Object> objects = new ArrayList<>();
         // These arguments like "Chisinau Airport" can go here due to the new Object java class created with @param string1,@param string2 ,@param imageResourceId
-        objects.add(new Object("Chisinau Airport","Moldova's main international airport, located 13 km (8.1 mi) southeast of the centre of Chisinau.\nIt serves as headquarters for Air Moldova, the country's national airline. " +
-                "Chisinau International Airport offers connections to major destinations, within Europe and Asia.\nAddress: Aeroportului 80/30 Str, Chisinau MD 2026\nTel: +373 22 525 111",R.drawable.chisinau_airport));
+        objects.add(new Object("Chisinau Airport", getString(R.string.airport_description), R.drawable.chisinau_airport));
 
         ObjectAdapter adapter = new ObjectAdapter(this,objects);
 
--- app/src/main/java/com/example/android/moldovatourguide/WineTourActivity.java
+++ app/src/main/java/com/example/android/moldovatourguide/WineTourActivity.java
@@ -17,19 +17,12 @@ public class WineTourActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
-        setTitle("Wine Tour");
+        setTitle(getString(R.string.wine_tour));
         final ArrayList<Object> objects = new ArrayList<>();
 
-        objects.add(new Object("Purcari Winery", "Purcari Winery situated 75km from Chisinau city centre is known to the whole world for its premium wines, which have received awards and praise from international competitions like Decanter, Challenge International du Vin, Concours Mondial de Bruxelles, " +
-                "International Wine and Spirit Competition as well as the legendary patronage of the royal court of Great Britain.\nAddress: Purcari, Ștefan-Vodă\n" +
-                "Tel:(+373) 24 230 411", R.drawable.purcari_winery));
-        objects.add(new Object("Cricova Winery", "\"Cricova\" JSC is a pearl of the Moldovan wine that represents an unique underground city well-known all over the world by its huge labyrinths, and especially by its excellent wines. \"Cricova\" is the first winery in Moldova which makes sparkling wine as it was prepared by " +
-                "the monk - wine maker Pierre Perignon. The technology \"Méthode Traditionnelle\" is a complex and thin process, consisting in secondary, " +
-                "in-bottle fermentation and the subconsequent maturation \"cuvée \"\nAddress: Cricova, str. Petru Ungureanu, 1\nTel: +373 22 604 027", R.drawable.cricova_cellar_winery));
-        objects.add(new Object("Milestii Mici Winery", "Surrounded by a romantic aura, that natural wine depository – the biggest in the world – has appeared in the limestone galleries at the depth of 30-85 meters in the 70’s of the previous century.The wines that are stored here are the products of the harvest of different years, " +
-                "between 1986 and 1991. Spirit of the past times is felt in this underground wine city growing out of the large tunnels. " +
-                "Natural limestone galleries with total length of 200 km may be passed by transport. And the streets of the underground wine city, placed on a surface of 55 km, are full of oak buts that are standing there as guardians on the watch .\nAddress: Milestii Mici Village, Ialoveni District" +
-                ", Republic of Moldova, MD-6819.\nTel: +373 22 382 333", R.drawable.milestii_mici_winery));
+        objects.add(new Object("Purcari Winery", getString(R.string.purcari_winery_description), R.drawable.purcari_winery));
+        objects.add(new Object("Cricova Winery", getString(R.string.cricova_winery_description), R.drawable.cricova_cellar_winery));
+        objects.add(new Object("Milestii Mici Winery", getString(R.string.milestii_winery_description), R.drawable.milestii_mici_winery));
 
         ObjectAdapter adapter = new ObjectAdapter(this, objects);
         ListView listView = (ListView) findViewById(R.id.list_item);
